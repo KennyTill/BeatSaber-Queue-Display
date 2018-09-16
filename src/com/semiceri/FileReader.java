f5e3df8a40a19ab8bfe0538e338c45cdeeb80dd5
Kenneth Till <kenneth.g.till@gmail.com>
2018-09-15 21:55:35 -0700

    adding in file reader class instead of doing it all in main like a bad idea

---
+++ src/com/semiceri/FileReader.java
@@ -0,0 +1,30 @@
+package com.semiceri;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileReader {
+
+    final String fileLocation;
+    final File file;
+
+    public FileReader(String fileLocation) {
+        this.fileLocation = fileLocation;
+        this.file = new File(fileLocation);
+    }
+
+    public String[] readFile(){
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            byte[] data = new byte[(int) file.length()];
+            fis.read(data);
+            fis.close();
+            String fileData = new String(data, "UTF-8");
+            String[] songArray = fileData.split("/n");
+            return songArray;
+        } catch (IOException ex){
+            return new String[]{ex.getMessage()};
+        }
+    }
+}
--- src/com/semiceri/Main.java
+++ src/com/semiceri/Main.java
@@ -20,17 +20,15 @@ public class Main {
         queueFrame.setVisible(true);
 
         try {
-
-            File file = new File(queueFileLocation);
+            FileReader queueReader = new FileReader(queueFileLocation);
 
 
             while (true) {
-                FileInputStream fis = new FileInputStream(file);
-                byte[] data = new byte[(int) file.length()];
-                fis.read(data);
-                fis.close();
-                String fileData = new String(data, "UTF-8");
-                playerFrame.updateLabel(fileData); // need to split things, this is just for testing
+                String[] queueData = queueReader.readFile();
+
+                //lets split this up and refactor it later.
+
+                playerFrame.updateLabel(queueData[0]);
                 Thread.sleep(5000);
             }
 
