444703979627cdfac7bd23a7731ffbaeb4a07537
Kenneth Till <kenneth.g.till@gmail.com>
2018-09-15 21:46:54 -0700

    messing around with reading a queue file

--- src/com/semiceri/Main.java
+++ src/com/semiceri/Main.java
@@ -1,21 +1,39 @@
 package com.semiceri;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        if (args.length == 0){
+            System.out.println("No queue file specified");
+        }
+
+        String queueFileLocation = args[0];
+        TransparentFrame playerFrame = new TransparentFrame("Awaiting Player", 75);
+        playerFrame.setVisible(true);
+
+        TransparentFrame queueFrame = new TransparentFrame("", 30);
+        queueFrame.setLocation(960, 100);
+        queueFrame.setVisible(true);
+
         try {
-            TransparentFrame playerFrame = new TransparentFrame("Awaiting Player", 75);
-            playerFrame.setVisible(true);
 
-            TransparentFrame queueFrame = new TransparentFrame("Some Info", 30);
-            queueFrame.setLocation(960, 100);
-            queueFrame.setVisible(true);
+            File file = new File(queueFileLocation);
 
 
-            while (true) { //TODO: read/update loop here from queue file
+            while (true) {
+                FileInputStream fis = new FileInputStream(file);
+                byte[] data = new byte[(int) file.length()];
+                fis.read(data);
+                fis.close();
+                String fileData = new String(data, "UTF-8");
+                playerFrame.updateLabel(fileData); // need to split things, this is just for testing
                 Thread.sleep(5000);
-                playerFrame.updateLabel("Player Name");
             }
+
         } catch (Exception ex){
             System.out.println(ex);
         }
